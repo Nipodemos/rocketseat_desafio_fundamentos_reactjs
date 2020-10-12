ae720417995f9e65deef232417c2e32282167d5a
Nipodemos <nipodemos13@gmail.com>
2020-10-12 19:54:05 -0300

    feat: list the transactions and show in the header

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,10 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+  const formatted = formatter.format(value);
+  return formatted;
+}; // TODO
 
 export default formatValue;
