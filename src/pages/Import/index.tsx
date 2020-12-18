bd168993146c042e3ef7e98b319d6608974dd97e
Nipodemos <nipodemos13@gmail.com>
2020-12-18 19:22:29 -0300

    importação de arquivo agora está funcional

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (uploadedFiles.length) {
+      data.append('file', uploadedFiles[0].file);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
+      try {
+        await api.post('/transactions/import', data);
+      } catch (err) {}
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles(
+      files.map(
+        (file): FileProps => ({
+          file,
+          name: file.name,
+          readableSize: String(file.size),
+        }),
+      ),
+    );
   }
 
   return (
